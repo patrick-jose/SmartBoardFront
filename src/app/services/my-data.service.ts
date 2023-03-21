2fb77a0fe3b02d6587996b46a49ecfa8a7b60200
patric-jose <patric-jose@hotmail.com>
2023-03-21 01:25:05 -0300

    API port update and dockerfile creation

--- src/app/services/my-data.service.ts
+++ src/app/services/my-data.service.ts
@@ -15,48 +15,48 @@ export class MyDataService {
   constructor(private http: HttpClient) { }
 
   getBoards() {
-    return this.http.get<Array<Board>>('https://localhost:7068/api/Board/GetAllActiveBoards');
+    return this.http.get<Array<Board>>('https://localhost:8081/api/Board/GetAllActiveBoards');
   }
   postBoard(board: Board) {
-    return this.http.post('https://localhost:7068/api/Board', board);
+    return this.http.post('https://localhost:8081/api/Board', board);
   }
   getAllSections() {
-    return this.http.get<Array<Section>>('https://localhost:7068/api/Section/GetAllSections');
+    return this.http.get<Array<Section>>('https://localhost:8081/api/Section/GetAllSections');
   }
   postSection(section: SectionDTO) {
-    return this.http.post('https://localhost:7068/api/Section', section);
+    return this.http.post('https://localhost:8081/api/Section', section);
   }
   postComment(comment: CommentDTO) {
-    return this.http.post('https://localhost:7068/api/Comment', comment);
+    return this.http.post('https://localhost:8081/api/Comment', comment);
   }
   postTask(task: TaskDTO) {
-    return this.http.post('https://localhost:7068/api/Task', task);
+    return this.http.post('https://localhost:8081/api/Task', task);
   }
   getSectionById(sectionId : number) {
-    return this.http.get<Section>('https://localhost:7068/api/Section/GetSectionById?id=' + sectionId);
+    return this.http.get<Section>('https://localhost:8081/api/Section/GetSectionById?id=' + sectionId);
   }
   getAllSectionsByBoardId(boardId: number) {
-    return this.http.get<Array<Section>>('https://localhost:7068/api/Section/GetAllActiveSectionsByBoardId?boardId=' + boardId);
+    return this.http.get<Array<Section>>('https://localhost:8081/api/Section/GetAllActiveSectionsByBoardId?boardId=' + boardId);
   }
   getAllTasksBySectionId(sectionId: number) {
-    return this.http.get<Array<Task>>('https://localhost:7068/api/Task/GetAllTasksBySectionId?sectionId=' + sectionId);
+    return this.http.get<Array<Task>>('https://localhost:8081/api/Task/GetAllTasksBySectionId?sectionId=' + sectionId);
   }
   getAllCommentsByTaskId(sectionId: number) {
-    return this.http.get<Array<CommentDTO>>('https://localhost:7068/api/Comment/GetCommentByTaskId?taskId=' + sectionId);
+    return this.http.get<Array<CommentDTO>>('https://localhost:8081/api/Comment/GetCommentByTaskId?taskId=' + sectionId);
   }
   getAllUsers() {
-    return this.http.get<Array<User>>('https://localhost:7068/api/User/GetAllUsers');
+    return this.http.get<Array<User>>('https://localhost:8081/api/User/GetAllUsers');
   }
   getUserById(userId: number) {
-    return this.http.get<User>('https://localhost:7068/api/User?id=' + userId);
+    return this.http.get<User>('https://localhost:8081/api/User?id=' + userId);
   }
   getUserId(name: String, password: String) {
-    return this.http.get<User>('https://localhost:7068/api/User/GetDetails?userName=' + name + '&password=' + password);
+    return this.http.get<User>('https://localhost:8081/api/User/GetDetails?userName=' + name + '&password=' + password);
   }
   checkCredentials(userName: String, password: String){
-    return this.http.get<boolean>('https://localhost:7068/api/User/CheckCredentials?userName=' + userName + '&password=' + password);
+    return this.http.get<boolean>('https://localhost:8081/api/User/CheckCredentials?userName=' + userName + '&password=' + password);
   }
   getStatusHistoryByTaskId(taskId: number) {
-    return this.http.get<Array<StatusHistory>>('https://localhost:7068/api/StatusHistory/GetStatusHistoryByTaskId?taskId=' + taskId);
+    return this.http.get<Array<StatusHistory>>('https://localhost:8081/api/StatusHistory/GetStatusHistoryByTaskId?taskId=' + taskId);
   }
 }
